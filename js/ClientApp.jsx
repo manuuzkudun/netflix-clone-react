e7dcd9ab5ef9a83821a7cfdda96c015d04d6b511
Manu Uzkudun <uzkudun@mdpi.com>
2016-10-21 14:40:49 +0200

    Layout component added

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Landing from './Landing'
-import { Router, Route, hashHistory } from 'react-router'
+import Layout from './Layout'
 import Search from './Search'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 ReactDOM.render(<App />, document.getElementById('app'))
--- js/Search.jsx
+++ js/Search.jsx
@@ -1,18 +1,12 @@
 import React from 'react'
 import data from '../public/data'
+import ShowCard from './ShowCard'
 
 const Search = () => (
   <div className='container'>
     <div className='shows'>
-      {data.shows.map(show => (
-        <div className='show-card'>
-          <img src={`public/img/posters/${show.poster}`} className='show-card-img' />
-          <div className='show-card-text'>
-            <h3 className='show-card-title'>{show.title}</h3>
-            <h4 className='show-card-year'>({show.year})</h4>
-            <p className='show-card-description'>{show.description}</p>
-          </div>
-        </div>
+      {data.shows.map((show) => (
+        <ShowCard {...show} key={show.imdbID} />
       ))}
     </div>
   </div>

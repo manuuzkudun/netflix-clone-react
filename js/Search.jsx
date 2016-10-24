8c5b83fe8d56d30cad7ff35da838e93f85604b5e
Manu Uzkudun <manuuzkudun@gmail.com>
2016-10-24 09:38:58 +0200

    Search functionality implemented

--- js/Search.jsx
+++ js/Search.jsx
@@ -2,14 +2,37 @@ import React from 'react'
 import data from '../public/data'
 import ShowCard from './ShowCard'
 
-const Search = () => (
-  <div className='container'>
-    <div className='shows'>
-      {data.shows.map((show) => (
-        <ShowCard {...show} key={show.imdbID} />
-      ))}
-    </div>
-  </div>
-)
+export default class Search extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+    this.handleSearchTermEvent = this.handleSearchTermEvent.bind(this)
+  }
 
-module.exports = Search
+  handleSearchTermEvent (event) {
+    this.setState({searchTerm: event.target.value})
+  }
+
+  render () {
+    return (
+      <div className='container'>
+        <header className='header'>
+          <h1 className='brand'>svideo</h1>
+          <input onChange={this.handleSearchTermEvent}
+            value={this.state.searchTerm}
+            className='search-input'
+            type='text' placeholder='Search' />
+        </header>
+        <div className='shows'>
+          {data.shows.filter((show) =>
+            `${show.title} ${show.description}`.toUpperCase().indexOf(this.state.searchTerm.toUpperCase()) >= 0)
+            .map((show) => (
+              <ShowCard {...show} key={show.imdbID} />
+          ))}
+        </div>
+      </div>
+    )
+  }
+}
